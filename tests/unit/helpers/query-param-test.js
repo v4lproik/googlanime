67c5fe8db185fd78a3ed778987ec0aa88542963f
Joel Rousseau <v4lproik@gmail.com>
2015-04-27 14:39:13 +0200

    Added queryParam helper to display queryParams[variable] (+1 squashed commit) Squashed commits: [74b529a] Added queryParam helper to display queryParams[variable]

---
+++ tests/unit/helpers/query-param-test.js
@@ -0,0 +1,11 @@
+import {
+  queryParam
+} from 'googlanime/helpers/query-param';
+
+module('QueryParamHelper');
+
+// Replace this with your real tests.
+test('it works', function() {
+  var result = queryParam(42);
+  ok(result);
+});
