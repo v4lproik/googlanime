2290b763ed143a6c7cbba4a4fd66f7378af81089
Joel Rousseau <v4lproik@gmail.com>
2015-04-29 16:59:54 +0200

    Refactor search display so it sticks more with Ember.JS framework

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -8,11 +8,18 @@ export default Ember.Controller.extend({
 
   layoutList: false,
 
+  animeSelected: null,
+
   actions: {
     search: function() {
       this.set('query', this.get('searchValue'));
       console.debug("changing search value");
       window.location.replace("/search?query=" + this.get('searchValue'));
+    },
+
+    updateSeriePanel: function(anime) {
+      console.debug("changing serie panel value" + anime.get("titles.romaji"));
+      this.set("animeSelected", anime);
     }
   }
 });
