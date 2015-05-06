9367a76d7f75f5c8db65020992db80b73e593245
Joel Rousseau <v4lproik@gmail.com>
2015-05-06 13:56:49 -0400

    Add frontend folder to the scope

--- app/adapters/anime.js
+++
@@ -1,9 +0,0 @@
-import DS from 'ember-data';
-import App from 'ember';
-import config from '../config/environment';
-
-export default DS.RESTAdapter.extend({
-  host: config.backend.host,
-  namespace: config.backend.namespace
-});
-
--- app/helpers/query-param.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export function queryParam(input, controller) {
-  return controller.get(input);
-};
-
-export default Ember.Handlebars.makeBoundHelper(queryParam);
