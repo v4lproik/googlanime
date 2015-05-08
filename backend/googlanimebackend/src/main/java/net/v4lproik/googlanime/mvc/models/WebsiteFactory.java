65dde2012cb6cb69faeb4c22dff5c41822a62005
Joel Rousseau <v4lproik@gmail.com>
2015-05-08 14:13:47 -0400

    Add @Service for autowiring

--- backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/models/WebsiteFactory.java
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/models/WebsiteFactory.java
@@ -1,12 +1,13 @@
 package net.v4lproik.googlanime.mvc.models;
 
 import net.v4lproik.googlanime.service.api.MyAnimeList;
-import net.v4lproik.googlanime.service.api.Website;
 import net.v4lproik.googlanime.service.api.WebsiteAbstract;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by joel on 08/05/2015.
  */
+@Service
 public class WebsiteFactory {
     public WebsiteAbstract getWebsite(String websiteType){
         if(websiteType == null){
