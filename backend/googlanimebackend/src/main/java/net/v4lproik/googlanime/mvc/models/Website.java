71d62d3d3f2d5b82ef09c6ba7e2bbe9621c955be
Joel Rousseau <v4lproik@gmail.com>
2015-05-28 09:30:02 +0200

    Improve enum functions

--- backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/models/Website.java
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/models/Website.java
@@ -1,10 +1,35 @@
 package net.v4lproik.googlanime.mvc.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by joel on 08/05/2015.
  */
 
 public enum Website{
-    MAL
+    MAL(0);
+
+    private int id;
+
+    Website(int i) {
+
+    }
+
+    private static final Map<String, Website> INDEX = new HashMap() {{
+        for (Website item : Website.values()) put(item.id, item);
+    }};
+
+    public static Website getTypeFromId(int value) {
+        return INDEX.containsKey(value) ? INDEX.get(value) : null;
+    }
+
+    public static Website containsValue(String value) {
+        try {
+            return Website.valueOf(value);
+        } catch (IllegalArgumentException e){
+            return null;
+        }
+    }
 }
 
