e3773bbda0a612d702d9b92b459944c462627914
Joel Rousseau <v4lproik@gmail.com>
2015-05-30 22:05:17 +0200

    Add character to the list of information that can be collected

---
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/service/api/MyAnimeListCharacter.java
@@ -0,0 +1,57 @@
+package net.v4lproik.googlanime.service.api;
+
+/**
+ * Created by joel on 29/05/2015.
+ */
+public class MyAnimeListCharacter {
+
+    private Integer id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String japaneseName;
+
+    private String role;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getJapaneseName() {
+        return japaneseName;
+    }
+
+    public void setJapaneseName(String japaneseName) {
+        this.japaneseName = japaneseName;
+    }
+}
