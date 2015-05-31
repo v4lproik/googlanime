9b7118b27de7a089f74dbb4f897a653e4ee8c77f
Joel Rousseau <v4lproik@gmail.com>
2015-05-31 11:37:20 +0200

    Add new tests

--- backend/googlanimebackend/src/test/java/net/v4lproik/googlanime/service/api/MyAnimeListUTest.java
+++ backend/googlanimebackend/src/test/java/net/v4lproik/googlanime/service/api/MyAnimeListUTest.java
@@ -8,6 +8,7 @@ import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.doReturn;
@@ -19,7 +20,7 @@ public class MyAnimeListUTest {
     private MyAnimeList myAnimeList;
 
     @Test
-    public void testCrawlById() throws Exception {
+    public void testCrawlById_withGoodOptions_shouldBeOK() throws Exception {
         // Given
         ImportOptions options = new ImportOptions(11, "manga", false);
         String type = options.getType();
@@ -36,5 +37,44 @@ public class MyAnimeListUTest {
 
         //Then
         assertEquals("Naruto", response.getTitle());
+    }
+
+    @Test(expected = IOException.class)
+    public void testCrawlById_withGoodOptions_withNoDataToCrawl_shouldThrowIOException() throws Exception {
+        // Given
+        ImportOptions options = new ImportOptions(11, "manga", false);
+        String type = options.getType();
+        final String id = options.getId().toString();
+        final String url = MyAnimeList.DOMAIN + type + "/" + id;
+
+
+        doReturn(null).when(myAnimeList).getResultFromJSoup(url, type);
+
+        // When
+        MyAnimeListAnime response = myAnimeList.crawlById(options);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCrawlById_withMissingOptions_shouldThrowIllegalArgumentException() throws Exception {
+        // Given
+        ImportOptions options = new ImportOptions(11, null, false);
+        String type = options.getType();
+        final String id = options.getId().toString();
+        final String url = MyAnimeList.DOMAIN + type + "/" + id;
+
+        // When
+        MyAnimeListAnime response = myAnimeList.crawlById(options);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testCrawlById_withEmptyOptions_shouldThrowIllegalArgumentException() throws Exception {
+        // Given
+        ImportOptions options = new ImportOptions(11, "", false);
+        String type = options.getType();
+        final String id = options.getId().toString();
+        final String url = MyAnimeList.DOMAIN + type + "/" + id;
+
+        // When
+        MyAnimeListAnime response = myAnimeList.crawlById(options);
     }
 }
