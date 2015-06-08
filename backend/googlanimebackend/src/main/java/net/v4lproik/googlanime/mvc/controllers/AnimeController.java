58da10d41035fc12867dd23cc15176ae48397cbb
Joel Rousseau <v4lproik@gmail.com>
2015-06-08 23:22:16 +0200

    [backend] Add render variable

--- backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/controllers/AnimeController.java
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/mvc/controllers/AnimeController.java
@@ -1,6 +1,7 @@
 package net.v4lproik.googlanime.mvc.controllers;
 
 import net.v4lproik.googlanime.mvc.models.JSONResponse;
+import net.v4lproik.googlanime.mvc.models.Website;
 import net.v4lproik.googlanime.service.api.AnimeService;
 import net.v4lproik.googlanime.service.api.myanimelist.models.MyAnimeListAnimeDependency;
 import org.apache.log4j.Logger;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -21,16 +23,28 @@ public class AnimeController {
     @Autowired
     private AnimeService service;
 
-    @RequestMapping(value = "/mal", method = RequestMethod.GET, params = {"query", "fields"})
+    @RequestMapping(value = "", method = RequestMethod.GET, params = {"query", "fields", "render"})
     @ResponseStatus(value = HttpStatus.OK)
     @ResponseBody
     public JSONResponse list(@RequestParam(value = "query", required = true) String query,
-                             @RequestParam(value = "fields", required = true) String[] fields) throws IOException {
+                             @RequestParam(value = "fields", required = true) String[] fields,
+                             @RequestParam(value = "render", required = false) String render) throws IOException {
 
+        log.debug(String.format("/animes?query=%s&fields=%s&render=%s", query, Arrays.asList(fields),render));
+
+        Class<? extends MyAnimeListAnimeDependency> toCast = MyAnimeListAnimeDependency.class;
         JSONResponse response = new JSONResponse();
 
+        if (Website.containsValue(render) != null){
+            switch (Website.containsValue(render)) {
+                case MAL:
+                    toCast = MyAnimeListAnimeDependency.class;
+                    break;
+            }
+        }
+
         try {
-            List<?> animes = service.find(query, fields, MyAnimeListAnimeDependency.class);
+            List<?> animes = service.find(query, fields, toCast);
             response.setAnimes(animes);
         } catch (Exception e) {
             log.error(e.getMessage());
