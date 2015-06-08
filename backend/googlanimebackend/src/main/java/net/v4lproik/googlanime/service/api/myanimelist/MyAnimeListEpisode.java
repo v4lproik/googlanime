c2fbb807d6a75a744b8b017835e9ed9a51bd67b6
Joel Rousseau <v4lproik@gmail.com>
2015-06-08 08:55:46 +0200

    [backend] New MyAnimeList model

---
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/service/api/myanimelist/MyAnimeListEpisode.java
@@ -0,0 +1,37 @@
+package net.v4lproik.googlanime.service.api.myanimelist;
+
+/**
+ * Created by joel on 18/05/2015.
+ */
+public class MyAnimeListEpisode {
+
+    private Integer id;
+
+    private String title;
+
+    private String date;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
