3f173137d9e36c51c3ab01dd2a1de39e9b1b2fa9
v4lproik <v4lproik@gmail.com>
2015-06-11 22:42:20 +0200

    [backend] Elasticsearch host is now using the ip/host provided in default.properties

--- backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/Config.java
+++ backend/googlanimebackend/src/main/java/net/v4lproik/googlanime/Config.java
@@ -5,17 +5,25 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
 
+    @Value("${elasticsearch.host}")
+    public String ELASTICSEARCH_HOST;
+
     @Bean(destroyMethod = "close")
     public TransportClient node() {
         Settings settings = ImmutableSettings.settingsBuilder()
                 .put("cluster.name", "elasticsearch").build();
-        return new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress("sandbox", 9300));
+
+        if (ELASTICSEARCH_HOST == null)
+            throw new IllegalArgumentException("The environment variable ELASTICSEARCH_HOST has not been set");
+
+        return new TransportClient(settings).addTransportAddress(new InetSocketTransportAddress(ELASTICSEARCH_HOST, 9300));
     }
 
     @Bean
