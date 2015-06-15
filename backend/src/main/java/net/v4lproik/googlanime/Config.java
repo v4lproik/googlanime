e0abd80ebfe0ac16ff3abd747c49fe580b496ae0
v4lproik <v4lproik@gmail.com>
2015-06-15 22:55:29 +0200

    [backend] Create multiple property files environment

--- backend/src/main/java/net/v4lproik/googlanime/Config.java
+++ backend/src/main/java/net/v4lproik/googlanime/Config.java
@@ -5,20 +5,30 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.ImmutableSettings;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.springframework.beans.factory.annotation.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.core.env.Environment;
 
-@Component
+@Configuration
+@PropertySources(@PropertySource("classpath:properties/app-${spring.profiles.active}.properties"))
 public class Config {
 
-    @Value("${elasticsearch.host}")
-    public String ELASTICSEARCH_HOST;
+    private static final Logger LOG = LoggerFactory.getLogger(Config.class);
+
+    @Autowired
+    private Environment env;
 
     @Bean(destroyMethod = "close")
     public TransportClient node() {
         Settings settings = ImmutableSettings.settingsBuilder()
                 .put("cluster.name", "elasticsearch").build();
+
+        final String ELASTICSEARCH_HOST = env.getProperty("elasticsearch.host");
 
         if (ELASTICSEARCH_HOST == null)
             throw new IllegalArgumentException("ElasticSearch host cannot be found. Check that the active profile provided a file that contains the variable elasticsearch.host ");
