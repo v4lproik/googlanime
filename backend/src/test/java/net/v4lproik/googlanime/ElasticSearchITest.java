4e49a65210df996aa10f5a6317a2bd72f03285d9
v4lproik <v4lproik@gmail.com>
2015-06-21 21:06:00 +0200

    [test] Update ElasticSearch integration tests

--- backend/src/test/java/net/v4lproik/googlanime/ElasticSearchITest.java
+++ backend/src/test/java/net/v4lproik/googlanime/ElasticSearchITest.java
@@ -1,7 +1,9 @@
 package net.v4lproik.googlanime;
 
 import junit.framework.TestCase;
-import org.elasticsearch.client.transport.TransportClient;
+import net.v4lproik.googlanime.client.elasticsearch.ElasticsearchIndexInitializer;
+import net.v4lproik.googlanime.client.elasticsearch.ElasticsearchTestConfiguration;
+import org.elasticsearch.client.Client;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,23 +16,34 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(
         classes = {
-                Config.class
+                ElasticsearchTestConfiguration.class
         })
 @ImportResource("classpath*  : application-context.xml")
 public class ElasticSearchITest extends TestCase {
 
     @Autowired
-    private TransportClient client;
+    private ElasticsearchIndexInitializer elasticsearchIndexInitializer;
+
+    @Autowired
+    private Client client;
 
     @Before
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
+        elasticsearchIndexInitializer.createIndex();
     }
+
+
 
     @Test
     public void elasticsearch_getInformation_fromAnimesIndex_shouldBeOk(){
-        client.prepareGet("animes", "anime", "1")
-                .execute()
-                .actionGet();
+        try {
+            client.prepareGet("animes", "anime", "1")
+                    .execute()
+                    .actionGet();
+        }catch (Exception e){
+            fail();
+        }
+
     }
 }
